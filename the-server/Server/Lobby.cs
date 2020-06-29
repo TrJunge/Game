6f5bd975e055896f86fa8713ac84b119f5d29e19
InteractiveGameBY <interactivegameby@gmail.com>
2020-06-29 13:40:29 +0300

    +ReadMe. Create server

---
+++ the-server/Server/Lobby.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Lobby
+    {
+        public string name;
+        public int countPlayers;
+        public string teamName1;
+        public string teamName2;
+        //public Team team1;
+        //public Team team2;
+        public int timeRound;
+        public int score;
+        public List<Player> players = new List<Player>();
+        public string creator;
+
+
+        public Lobby(string name, string nameLibrary, int countPlayers, /*Team team1, Team team2,*/ int timeRound, int wordsForWin, bool minus, string creator)
+        {
+            this.name = name;
+            this.countPlayers = countPlayers;
+            this.timeRound = timeRound;
+            //this.team1 = team1;
+            //this.team2 = team2;
+            this.creator = creator;
+           //unusedWords = Database.getWordsFormWordbook(nameLibrary);
+        }
+
+        public string info()
+        {
+            return name + "," + countPlayers.ToString() + "," + timeRound.ToString();
+        }
+
+        public void addInLobby(Player player)
+        {
+            players.Add(player);
+        }
+
+        public void removeInLobby(Player player)
+        {
+            players.Remove(player);
+        }
+
+        //public void subscribeTeam(Team team, string nick)
+        //{
+        //    foreach (Player player in players)
+        //    {
+        //        if (player.nickname == nick) team.subscribe(player);
+        //    }
+        //}
+
+        //public void unsubscribeTeam(Team team, string nick)
+        //{
+        //    foreach (Player player in players)
+        //    {
+        //        if (player.nickname == nick) team.unsubscribe(player);
+        //    }
+        //}
+
+        //public string TeamInfo()
+        //{
+        //    return team1.playerInTeam() + "," + team2.playerInTeam() + "teamInfo";
+        //}
+    }
+}
---
+++ the-server/Server/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public class Player
+    {
+        public Socket socket;
+        //public Team team;
+        public string nickname;
+
+        public Player(Socket socket, string nickname)
+        {
+            this.socket = socket;
+            this.nickname = nickname;
+        }
+
+        /*public void subscribeToTeam(Team team)
+        {
+            this.team = team;
+        }*/
+
+        //public string printInfoTeam()
+        //{
+        //    return team.printName();
+        //}
+
+        public string printPlayerInfo()
+        {
+            return nickname + ","; //+ team.name;
+        }
+    }
+}
