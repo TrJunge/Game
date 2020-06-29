3934e952de439f6c8bd13d946af445b93d2e65b0
Vlad Artyomkin <artyomkinvlad@yandex.by>
2020-06-29 10:48:00 +0300

    Character

---
+++ the-game/Assets/Scripts/Character/Bullet.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+public class Bullet : MonoBehaviour
+{
+    private GameObject parent;
+    public GameObject Parent { set { parent = value; }  get { return parent; } }
+
+    private float speed = 15.0F;
+    private Vector3 direction;
+    public Vector3 Direction { set { direction = value; } }
+
+    public Color Color
+    {
+        set { sprite.color = value; }
+    }
+
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        Destroy(gameObject, 2.5F);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Unit unit = col.GetComponent<Unit>();
+
+        if (col.tag == "Ground" || col.tag == "Platform" || col.tag == "Wall") Destroy(gameObject);
+
+        if (unit && unit.gameObject != parent)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ the-game/Assets/Scripts/Character/CharactersServerReceive.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactersServerReceive : MonoBehaviour
+{
+    public static GameObject Character;
+    Vector3 position;
+    string nickname;
+    private void Awake()
+    {
+        Character = Resources.Load("CharacterOnline (R)") as GameObject;
+    }
+    public void Start()
+    {
+        CreateCharacter();
+        Connection.Connect();
+        //Connection.Send(character.transform.position.ToString());
+
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Connection.position!="")
+        {
+            position = DeserializeVector3Array(Connection.position);
+            //position_result = new Vector3(float.Parse(position_value[0]));
+            Connection.position.Split(',')[3] = "";
+            //Connection.Receive();
+            
+            //Connection.Send("ss ");
+            //Debug.Log("CH pos " + Character.transform.position.ToString());
+            //position = Character.transform.position;
+            //Debug.Log("Pos " + position);
+        }
+    }
+
+    public void CreateCharacter()
+    {
+        Instantiate(Character);
+    }
+
+    public static Vector3 DeserializeVector3Array(string aData)
+    {
+        string[] values = aData.Split(',');
+        Vector3 result = new Vector3();
+        result = new Vector3(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2]));
+        return result;
+    }
+}
